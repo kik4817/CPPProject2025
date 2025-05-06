884ac22d78d6285964a6cd75ce96e6d73f192f13
kik4817 <kik4817@naver.com>
2025-05-06 17:47:31 +0900

    (2025-05-06) 수정

---
+++ storyteller/Console.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+class Console
+{
+public:
+	static void GoToXY(int x, int y)
+	{
+		COORD pos = { static_cast<short> (x), static_cast<short>(y) };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	}
+
+	static void CursorVisible(bool show)
+	{
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		CONSOLE_CURSOR_INFO cursorinfo;
+		GetConsoleCursorInfo(hConsole, &cursorinfo);
+		cursorinfo.bVisible = show;
+		SetConsoleCursorInfo(hConsole, &cursorinfo);
+};
+
